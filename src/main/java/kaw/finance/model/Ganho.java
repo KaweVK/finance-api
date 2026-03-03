08c38c80fba7e72c0420d26eba4d50b8d4fdd100
KaweVK <victorkawe07@gmail.com>
2026-03-03 08:58:29 -0300

    feat: adicionado ganho

---
+++ src/main/java/kaw/finance/model/Ganho.java
@@ -0,0 +1,41 @@
+package kaw.finance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "ganho")
+public class Ganho {
+
+    @Id
+    @Column(name = "id",  nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome",  nullable = false)
+    private String nome;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "valor",  nullable = false)
+    private BigDecimal valor;
+    @Column(name = "data")
+    private Date data;
+    @CreatedDate
+    @Column(name = "data_registro")
+    private LocalDateTime dataRegistro;
+    @LastModifiedDate
+    @Column(name = "data_update")
+    private LocalDateTime dataUpdate;
+
+
+}
