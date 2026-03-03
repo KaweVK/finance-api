00c24ce4e5fee45e071152c7c18c8884b96fb635
KaweVK <victorkawe07@gmail.com>
2026-03-02 21:39:38 -0300

    feat: gasto controller

---
+++ src/main/java/kaw/finance/controller/GastoController.java
@@ -0,0 +1,62 @@
+package kaw.finance.controller;
+
+import jakarta.transaction.Transactional;
+import kaw.finance.dto.RegisterGastoDto;
+import kaw.finance.dto.ResponseGastoDto;
+import kaw.finance.dto.mapper.GastoMapper;
+import kaw.finance.service.GastoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/gasto")
+public class GastoController {
+
+    @Autowired
+    private GastoService service;
+    @Autowired
+    private GastoMapper mapper;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findGasto(@PathVariable Long id) {
+        var gasto = service.findById(id);
+        return ResponseEntity.ok().body(gasto);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<ResponseGastoDto>> findAllGastos(
+            @RequestParam(value = "page", defaultValue = "0")
+            Integer page,
+            @RequestParam(value = "size", defaultValue = "10")
+            Integer size
+            ) {
+        Page<ResponseGastoDto> gastos = service.findAll(page, size);
+        return ResponseEntity.ok(gastos);
+    }
+
+    @PostMapping("/create")
+    @Transactional
+    public ResponseEntity<Object> createGasto(@RequestBody RegisterGastoDto dto) {
+        var gasto = service.createGasto(dto);
+        return ResponseEntity.ok().body(gasto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @Transactional
+    public ResponseEntity<Object> deleteGasto(@PathVariable Long id) {
+        service.deleteGasto(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update/{id}")
+    @Transactional
+    public ResponseEntity<Object> updateGasto(@PathVariable Long id, @RequestBody RegisterGastoDto dto) {
+        var gastoAtualizado = service.updateGasto(id, dto);
+        return ResponseEntity.ok().body(gastoAtualizado);
+    }
+
+}
