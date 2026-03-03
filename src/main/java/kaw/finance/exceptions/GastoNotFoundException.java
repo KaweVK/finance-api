6127a254456de4757e431ba4f626ba0412d15125
KaweVK <victorkawe07@gmail.com>
2026-03-02 21:56:24 -0300

    feat: adicionado exception para gasto não encontrado

---
+++ src/main/java/kaw/finance/exceptions/GastoNotFoundException.java
@@ -0,0 +1,7 @@
+package kaw.finance.exceptions;
+
+public class GastoNotFoundException extends RuntimeException {
+    public GastoNotFoundException(String message) {
+        super(message);
+    }
+}
